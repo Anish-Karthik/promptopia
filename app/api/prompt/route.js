07e1d03d924e3027c802897327f0f7588e6f5bcd
Anish Karthik <anishkarthika21cs@psnacet.edu.in>
2023-08-12 22:57:42 +0530

    implemented search feature

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,10 +1,28 @@
 import { connectToDatabase } from "@utils/database";
 import Prompt from "@models/prompt";
+import User from "@models/user";
 
 export const GET = async (req) => {
+  // read query params
   try {
     await connectToDatabase();
-    const prompts = await Prompt.find({}).populate('creator');
+    let prompts;
+    const search = req.nextUrl.searchParams.get('search');
+    if(search && search.length > 0) {
+      // check if search is in prompt or tag or username from creator
+      const creators = await User.find({ username: { $regex: search, $options: 'i' } }, { _id: 1 });
+      prompts = await Prompt.find({
+        $or: [
+          { prompt: { $regex: search, $options: 'i' } },
+          { tag: { $regex: search, $options: 'i' } },
+          { creator: { $in: creators } }
+        ]
+      }).populate('creator');
+
+
+    } else {
+      prompts = await Prompt.find({}).populate('creator');
+    }
     return new Response(JSON.stringify(prompts), {
       status: 200
     });
--- components/Feed.jsx
+++ components/Feed.jsx
@@ -5,8 +5,29 @@ import PromptCard from './PromptCard'
 const Feed = () => {
   const [searchText, setSearchText] = React.useState('')
   const [posts, setPosts] = React.useState([])
+  
+  async function fetchPromptData (search) {
+    try {
+      const res = await fetch(`/api/prompt?search=${search}`)
+      const data = await res.json()
+      if (JSON.stringify(posts) === JSON.stringify(data)) return
+      setPosts(data)
+    } catch (error) { 
+      console.log(error)
+    }
+  }
+
   const handleSearchChange =  (e) => {
-    setSearchText(e.target.value)    
+    setSearchText(e.target.value)
+    const search = e.target.value
+    // debounce the search function
+    debounce(() => {
+      // fetch the data
+      fetchPromptData(search)
+    }, 500)()    
+  }
+  const handleTagClick = (tag) => {
+    fetchPromptData(tag)
   }
   React.useEffect(() => {
     const fetchPromptData = async () => {
@@ -33,7 +54,7 @@ const Feed = () => {
           className="search_input peer"
         />
       </form>
-      <PromptCardList data={posts} handleTagClick={() => {}} />
+      <PromptCardList data={posts} handleTagClick={handleTagClick} />
     </section>
   )
 }
@@ -43,9 +64,37 @@ export default Feed
 function PromptCardList({ data, handleTagClick }) {
   return (
     <div className='mt-16 prompt_layout'>
-      {data.map((post) => (
-        <PromptCard key={post._id} post={post} handleTagClick />
-      ))}
+      
+      { data || data.length>1 ? data.map((post) => (
+          <PromptCard key={post._id} post={post} handleTagClick={handleTagClick} />
+        ))
+      : 
+      <div className='prompt_card'>
+        <div className='flex-center flex-col'>
+          <h1 className='text-center'>No prompts found</h1>
+          <p className='text-center'>Try searching for something else</p>
+        </div>
+      </div>
+      }
     </div>
   )
+}
+
+// A debounce function that returns a new function that can only be called once per given time period
+function debounce (func, delay) {
+  // Initialize a timer variable
+  let timer;
+  // Return the new function
+  return function () {
+    // Get the context and arguments of the function
+    let context = this;
+    let args = arguments;
+    // Clear the previous timer
+    clearTimeout (timer);
+    // Set a new timer
+    timer = setTimeout (function () {
+      // Call the original function with the context and arguments
+      func.apply (context, args);
+    }, delay); // Pass in the delay in milliseconds
+  };
 }
